546f6d409ffbc81cbababd8d2cd72811baf13164
bslsnvonderwense1989 <294205207@bildung.daa.de>
2024-10-24 10:14:57 +0200

    Server: add initial Server

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template, redirect, url_for, flash
 from flask_socketio import SocketIO, emit
 from settings import Settings
-
 import asyncio
 import os
+
+
 app = Flask(__name__)
+
 socketio = SocketIO(app)
 settings = Settings(socketio)
 #settings.socketio = socketio
@@ -32,6 +34,15 @@ def update_settings():
             settings.images.append(image_path)
     socketio.emit('settingsUpdate', settings.to_dict())
     return '', 200
+
+@app.route('/admin', methods=['GET','POST'])
+def admin_panel():
+    return render_template('admin.html')
+
+@app.route('/index', methods=['GET'])
+def index():
+    return render_template('index.html')
+
 @app.route('/start', methods=['GET'])
 def start_info_board():
     settings.info_board_running = True
@@ -54,14 +65,16 @@ async def send_data():
             }
             socketio.emit('dataUpdate', data)
         await asyncio.sleep(settings.update_interval)
-async def update_content(self):
+
+"""async def update_content(self):
     while True:
         html = await self.generators[self.current_index].generate_html()
         socketio.emit('htmlUpdate', {'html': html})
         self.current_index = (self.current_index + 1) % len(self.generators)
-        await asyncio.sleep(self.interval / 1000)
+        await asyncio.sleep(self.interval / 1000)"""
+
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
     loop.create_task(send_data())
     loop.create_task(settings.update_content())
-    socketio.run(app, port=3000)
+    socketio.run(app, allow_unsafe_werkzeug=True, port=3000)
